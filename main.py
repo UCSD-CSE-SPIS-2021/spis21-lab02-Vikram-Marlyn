ebde45f301eced892f3cf09624e794d2563f4d53
Vikram Venkatesh <88351984+vikwaran03@users.noreply.github.com>
2021-08-08 19:45:52 +0000

    Added getNumber function

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+def getNumber():
+  
+  number1 = ''
+  
+  symbols = input("Enter a digit: ")
+  
+  number = int(symbols)
+  
+  while number > 0:
+      number1 = number1 +str(number)
+    
+      symbols = input("Enter a digit: ")
+    
+      number = int(symbols)
+  
+  return number1
+  
+
 # The goal of this program is to practice Python constructs
 
 def sumTwo(a,b):
