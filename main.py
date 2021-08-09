a6086cdc13905a543170d9c4e212fb7d8bcd44b2
Vikram Venkatesh <88351984+vikwaran03@users.noreply.github.com>
2021-08-09 22:44:55 +0000

    Added sumDigits()

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def getNumber():
   number = int(symbols)
   
   while number > 0:
-      number1 = number1 +str(number)
+      number1 = number1 + str(number)
     
       symbols = input("Enter a digit: ")
     
@@ -15,6 +15,15 @@ def getNumber():
   
   return number1
   
+def sumDigits(x):
+
+   # You will need to complete this function
+  valueList = list(map(int, str(x)))
+  sum2 = sum(valueList)
+  print(sum2)
+
+
+
 
 # The goal of this program is to practice Python constructs
 
@@ -29,3 +38,13 @@ def sumTwo(a,b):
 x = sumTwo(3,5)
 
 print(x)
+
+
+value = str(1232747437)
+
+valueList = list(map(int,str(value)))
+
+
+print(valueList)
+
+
